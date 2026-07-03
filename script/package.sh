a35a78aae243a08226f3f32984fac28333f6b14f
hayashi <haygsiiii@gmail.com>
2026-07-03 13:42:51 +0900

    Fix DMG layout setup

--- script/package.sh
+++ script/package.sh
@@ -141,21 +141,19 @@ hdiutil attach -readwrite -noverify -noautoopen -mountpoint "$DMG_MOUNT_DIR" "$D
 
 osascript <<APPLESCRIPT
 tell application "Finder"
-  tell disk "$APP_NAME"
+  set dmgFolderAlias to POSIX file "$DMG_MOUNT_DIR" as alias
+  tell folder dmgFolderAlias
     open
     set current view of container window to icon view
     set toolbar visible of container window to false
     set statusbar visible of container window to false
     set bounds of container window to {120, 120, 700, 440}
-    set viewOptions to icon view options of container window
-    set arrangement of viewOptions to not arranged
-    set icon size of viewOptions to 96
-    set text size of viewOptions to 16
+    set icon size of icon view options of container window to 96
+    set text size of icon view options of container window to 16
     set position of item "$APP_NAME.app" to {180, 150}
     set position of item "Applications" to {420, 150}
-    close
-    open
     update without registering applications
+    close container window
   end tell
 end tell
 APPLESCRIPT
